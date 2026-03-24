f59d209d4c1ed184a1b1b564a22b04b66ebbf84e
MiguelitoBFMV <miguelvaras1999@gmail.com>
2026-03-24 00:03:15 -0300

    Create View

--- phrases/views.py
+++ phrases/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def main_page(request):
-    return render(request, "phrases/main_page.html")
+    texto = ""
+
+    if request.method=="POST":
+        texto = request.POST.get("frase_esp", "")
+    return render(request, "phrases/main_page.html", {"frase_esp": texto})
