675e96ceddf4a2afd7e9845afd5bbf5853974177
MiguelitoBFMV <miguelvaras1999@gmail.com>
2026-03-23 22:22:33 -0300

    Rename some files

--- phrases/admin.py
+++ phrases/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from prhases.models import Categoria, Frase
+from phrases.models import Categoria, Frase
 
 admin.site.register(Categoria)
 admin.site.register(Frase)
