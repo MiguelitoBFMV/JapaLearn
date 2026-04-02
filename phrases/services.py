3c91a14536dafcc3bae00a8fede73e3c3b979191
MiguelitoBFMV <miguelvaras1999@gmail.com>
2026-04-01 23:50:48 -0300

    Fix bug

--- phrases/services.py
+++ phrases/services.py
@@ -1,10 +1,8 @@
 from google.cloud import translate_v2 as translate
 
 def translate_text(texto_esp):
-
-    translate_client = translate.Client()
-
     try:
+        translate_client = translate.Client()
         return translate_client.translate(
             values = texto_esp,
             target_language="ja",
